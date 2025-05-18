a44f64986292672269f8f21657e998eceb58a7a5
Harald Schioberg <harald.schioeberg@gmail.com>
2025-05-17 22:33:40 -0700

    submake

--- Makefile
+++ Makefile
@@ -1,15 +1,2 @@
-include Makefile.mk
-
-TOPTARGETS := all clean
-SUBDIRS := $(wildcard */.)
-
-all: $(SUBDIRS) stl
-
-stl:
-	$(MAKE) -C stl stl
-
-$(TOPTARGETS): $(SUBDIRS)
-$(SUBDIRS): 
-	$(MAKE) -C $@ $(MAKECMDGOALS)
 
 .PHONY: $(TOPTARGETS) $(SUBDIRS) stl
---
+++ stl/Makefile.mk
@@ -0,0 +1,16 @@
+SCAD_DIR = $(abspath $(dir $(lastword $(MAKEFILE_LIST)))../scad)
+SCAD_FILES = $(wildcard $(SCAD_DIR)/*.scad)
+
+STL_FILES = $(shell [ -f configs.json ] && jq -r '.["parameterSets"].[].stl' < configs.json)
+
+stl: $(STL_FILES)
+
+$(STL_FILES): configs.json $(SCAD_FILES)
+	openscad --backend Manifold -p configs.json -P $(basename $(notdir $@)) -o $@ $(SCAD_DIR)/bricks.scad
+
+.PHONY: clean distclean
+clean: 
+	rm -f $(wildcard **/*.stl)
+
+distclean: clean
+	rm -f $(wildcard **/*.json)
