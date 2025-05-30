e2c0576e69121eb669a3a01962be7b306585aeb9
Harald Schioberg <harald.schioeberg@gmail.com>
2025-05-30 16:06:33 -0700

    logs

--- bin/configure.py
+++ bin/configure.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import datetime
 import copy
 import json
 import numpy as np
@@ -11,21 +12,10 @@ import stl
 import sys
 import pathlib
 
-from absl import app, flags
-from absl.logging import log, WARNING, INFO, DEBUG
+from absl import app, flags, logging
 from ruamel.yaml import YAML
 from string import Template
 from typing import Any, Optional, Dict, List
-
-
-# MODULES = { 
-#   'all': 'Configs for all bricks, plates, and tiles.',
-#   'bricks': 'Create the standard bricks.',
-#   'plates': 'Create the standard plates', 
-#   'walls': 'Create the  standard walls',
-#   'tiles': 'Create the standard tiles',
-# }
-
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('remix_input', None, 'Path glob to input stl files')
@@ -38,12 +28,12 @@ flags.DEFINE_string('confpath', str(pathlib.Path(__file__).parents[1].resolve().
 
 flags.DEFINE_string('output', '.', 'Output directory or "-" for dumping config to stdout.')
 flags.register_validator('output', lambda o: o == "-" or pathlib.Path(o).is_dir(), message='output must be "-" or an existing directory.')
+flags.DEFINE_string('logfile', 'configure.log', 'Location of the logfile')
 
 flags.DEFINE_bool('individual_json', True, 'Write configs as individual files, one per stl file.')
 flags.DEFINE_bool('combined_json', False, 'Write a single combined json config.')
 flags.DEFINE_bool('rename', True, 'Create output stl files with a consistent nameing pattern. If false: Retain original stl filename')
 flags.DEFINE_bool('makefile', True, "Create the Makefile(s)")
-
 
 flags.DEFINE_integer('max_size', 8, 'Create bricks up to this size.')
 flags.DEFINE_integer('max_r', 5, 'Create hex bricks up to radius r.')
@@ -68,6 +58,21 @@ include {includedMakefile}
 
 class ConfigNotFound(KeyError):
   pass
+
+
+class Log():
+  _LOGFD = None
+  _instance = None
+
+  def __new__(cls): 
+    if cls._instance is None:
+      cls._instance = super(Log, cls).__new__(cls)
+      cls._LOGFD = open(FLAGS.logfile, 'w')
+    return cls._instance
+  
+  def __call__(self, msg):
+    self._LOGFD.write(msg + '\n')
+
 
 class Brick:
 
@@ -135,7 +140,7 @@ class Config(object):
     else: 
       raise ValueError('%s: Unknown fileinfo.type %s', self.name, self.type)
 
-  def match2Brick(self, filename) -> Optional[Brick]:
+  def remix(self, filename) -> Optional[Brick]:
     """Match a name to the regex and return a resolved config dict or None."""
 
     file = pathlib.Path(filename)
@@ -161,13 +166,16 @@ class Config(object):
     
     for (k,v) in update.items():
         if type(v) is str:
-          update[k] = v.vformat(match.groupdict())
+          update[k] = v.format(**match.groupdict())
         else:
           update[k] = v
     brick.update(update)
       
-    log(DEBUG, '%s: match2Brick: %r', file, str(brick))
-    return brick
+    logging.debug('%s: match2Brick: %r', file, str(brick))
+    if brick.isValid():
+      return brick
+    else:
+      return None
   
 def generateBricks() -> Dict: 
 
@@ -204,6 +212,9 @@ def generateBricks() -> Dict:
 
 
 def writeJsonConfigs(bricks):
+  log = Log()
+  log('')
+
   if FLAGS.output == '-': 
     conf = copy.deepcopy(CUSTOMIZER_TMPL)
     conf['parameterSets'] = {n: b.config for (n,b) in bricks.items()}
@@ -228,20 +239,20 @@ def writeJsonConfigs(bricks):
 
       outpaths.update(list(brick.stl.parents))
 
-      log(DEBUG, '%s: %r', name, conf)
+      logging.debug('%s: %r', name, conf)
       try: 
         with open(jf, 'r') as jfd:
           oldConf = json.load(jfd)
       except: 
         oldConf = {}
       if (oldConf.get('parameterSets', {}).get(name, {}) == conf):
-        log(INFO, '%s: No changes, skipping updates.', name)
+        log(f'{jf}: No changes')
         continue
 
       cconf = copy.deepcopy(CUSTOMIZER_TMPL)
       cconf['parameterSets'][name] = conf
       
-      log(INFO, '%s: writing config %r', name, cconf)
+      log(f'{jf}: Updated')
       jf.parent.mkdir(parents=True, exist_ok=True)
       with open(jf, 'w') as jfd:
         json.dump(cconf, jfd, indent=2, sort_keys=True)
@@ -259,36 +270,39 @@ def writeJsonConfigs(bricks):
         out.write(MAKEFILE_TMPL.format(includedMakefile=inclMake))
 
 def main(argv): 
-  del argv
+
+  if FLAGS['verbosity'].using_default_value:
+    logging.set_verbosity(logging.WARNING)
 
   output = pathlib.Path(FLAGS.output)
+  log = Log()
 
   remix = []
+
+  log(f'{datetime.datetime.now().isoformat()}: {" ".join(sys.argv)}')
 
   for yml in [pathlib.Path(FLAGS.confpath).joinpath(f'{c}.yaml') for c in FLAGS.config]:
     try:
       config = Config(yml)
     except Exception as e:
-      log(WARNING, "%s: Error loading config: %r", yml, e)
+      logging.warning("%s: Error loading config: %r", yml, e)
       continue
     if config.name not in FLAGS.config:
-      log(WARNING, 'Unexpected config %s, skipping.')
+      logging.warning('Unexpected config %s, skipping.')
       continue
     if config.type == 'REMIX':
      remix.append(config)
 
   bricks = {}
-  # bricks.update(generateBricks())
-
-  # config = Config('rampage', regex=RAMPAGE_RE, conf=RAMPAGE_CONF)
-  # log(DEBUG, 'input: %s', FLAGS.input)
 
   for infile in pathlib.Path('.').glob(FLAGS.remix_input): 
     for config in remix:
-      brick = config.match2Brick(infile)
-      if brick.isValid():
-        bricks[brick.name] = brick
+      if (brick := config.remix(infile)):
+        bricks[brick.name] = brick 
+        log(f'{infile.name}: remixing as {brick.name}, config {config.name}')
         break
+      else:
+        log(f'{infile.name}: no matching remix config')
 
   writeJsonConfigs(bricks)
 
